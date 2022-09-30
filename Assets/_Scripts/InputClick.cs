e2a138af129d9c5c975a92c1ed56c2cc7b411017
Илья <72304063+ilua2013@users.noreply.github.com>
2022-10-01 02:43:28 +0300

    AddMovementSystem

---
+++ Assets/_Scripts/InputClick.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputClick : MonoBehaviour
+{
+    [SerializeField] private PlayerMover _playerMover;
+
+    private void OnValidate()
+    {
+        _playerMover = FindObjectOfType<PlayerMover>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+            ShootRaycast();
+    }
+
+    private void ShootRaycast()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 1000))
+        {
+            _playerMover.MoveTo(hit.point);
+            Debug.Log("Raycast Point" + hit.point);
+        }
+    }
+}
