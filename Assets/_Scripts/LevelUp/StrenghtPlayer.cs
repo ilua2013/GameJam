dc9a132b6b04118b0356a9595d849475fb541076
Ildar1982 <68797569+SalihIldr@users.noreply.github.com>
2022-10-02 03:59:09 +0500

    Update StrenghtPlayer.cs

--- Assets/_Scripts/LevelUp/StrenghtPlayer.cs
+++ Assets/_Scripts/LevelUp/StrenghtPlayer.cs
@@ -13,37 +13,7 @@ public class StrenghtPlayer : MonoBehaviour
     public event UnityAction PlayerDied;
     public event UnityAction<int> ChangedHealt;
 
-    private void OnEnable()
-    {
-        _maxHealt = _playerParameter.MaxHealt;
-        _currentHealt = _maxHealt;
-
-        _playerParameter.ChangedMaxHealt += IncreaseMaxHealt;
-    }
-
-    private void OnDisable()
-    {
-        _playerParameter.ChangedMaxHealt -= IncreaseMaxHealt;
-    }
-
-    public void TakeDamage(int damage)
-    {
-        _currentHealt -= damage;
-        ChangedHealt?.Invoke(_currentHealt);
-        if (_currentHealt <= 0)
-        {
-            PlayerDied?.Invoke();
-        }
-    }
-
-    public void RecoveryHealt(int healt)
-    {
-        if (_maxHealt - _currentHealt > healt)
-            _currentHealt += healt;
-        else
-            _currentHealt += _maxHealt - _currentHealt;
-        ChangedHealt?.Invoke(_currentHealt);
-    }
+    
 
     private void IncreaseMaxHealt(int maxHealt)
     {
