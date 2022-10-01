0818482e43821fb0a4901907671f2890019944b2
Ildar1982 <68797569+SalihIldr@users.noreply.github.com>
2022-10-01 23:20:52 +0500

    HealtPlayer

---
+++ Assets/_Scripts/LevelUp/HealtPlayer.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class HealtPlayer : MonoBehaviour
+{
+    [SerializeField] private PlayerParameter _playerParameter;
+
+    private int _currentHealt;
+    private int _maxHealt;
+
+    public event UnityAction PlayerDied;
+    public event UnityAction<int> ChangedHealt;
+
+    private void OnEnable()
+    {
+        _maxHealt = _playerParameter.MaxHealt;
+        _currentHealt = _maxHealt;
+
+        _playerParameter.ChangedMaxHealt += IncreaseMaxHealt; 
+    }
+
+    private void OnDisable()
+    {
+        _playerParameter.ChangedMaxHealt -= IncreaseMaxHealt;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        _currentHealt -= damage;
+        ChangedHealt?.Invoke(_currentHealt);
+        if (_currentHealt <= 0)
+        {
+            PlayerDied?.Invoke();
+        }
+    }
+
+    public void RecoveryHealt(int healt)
+    {
+        if (_maxHealt - _currentHealt > healt)
+            _currentHealt += healt;
+        else
+            _currentHealt += _maxHealt - _currentHealt;
+        ChangedHealt?.Invoke(_currentHealt);
+    }
+
+    private void IncreaseMaxHealt(int maxHealt)
+    {
+        _maxHealt = maxHealt;
+    }
+}
